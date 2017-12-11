9cd90299b63b2702f5cd302eb6c23a120af5740f
wangfuzhu <31799655+wangfuzhu@users.noreply.github.com>
2017-12-11 22:00:39 +0300

    Исправить ошибку

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -7,7 +7,7 @@ package lesson7.task1
 data class Cell(val row: Int, val column: Int)
 
 /**
- * Интерфейс, описывающий возможности матрицы. E = тип элемента матрицы
+ * Интерфейс, описывающий возможности матрицы. E = тип элемента
  */
 interface Matrix<E> {
     /** Высота */
@@ -38,32 +38,43 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    if (height <= 0 || width <= 0 ) throw IllegalArgumentException()
+    val matrix = MatrixImpl(height, width, e)
+    for (row in 0 until height) {
+        for (column in 0 until width) {
+            matrix[row,column] = e
+        }
+    }
+    return matrix
+}
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E> {
+    private val cellList = mutableListOf<E>()
+    init {
+        for (it in 0 until height * width){
+            cellList.add(e)
+        }
+    }
+    override fun get(row: Int, column: Int): E = cellList[row * width + column]
 
-    override val width: Int = TODO()
-
-    override fun get(row: Int, column: Int): E  = TODO()
-
-    override fun get(cell: Cell): E  = TODO()
+    override fun get(cell: Cell): E = cellList[cell.row * width + cell.column]
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        cellList[row * width + column] = value
     }
 
     override fun set(cell: Cell, value: E) {
-        TODO()
+        cellList[cell.row * width + cell.column] = value
     }
 
     override fun equals(other: Any?) = TODO()
 
-    override fun toString(): String = TODO()
+    override fun toString(): String = "MatrixImpl(height=$height, width=$width, cellList=$cellList)"
 }
 
